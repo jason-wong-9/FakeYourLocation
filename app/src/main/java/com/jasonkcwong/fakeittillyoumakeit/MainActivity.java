07fffbb419a38d2e0aa599c7c67e526cb183a32a
Jason Wong <jason_19960903@hotmail.com>
2016-08-03 18:08:35 -0700

    Create marker and updates according to current location

--- app/src/main/java/com/jasonkcwong/fakeittillyoumakeit/MainActivity.java
+++ app/src/main/java/com/jasonkcwong/fakeittillyoumakeit/MainActivity.java
@@ -1,55 +1,121 @@
 package com.jasonkcwong.fakeittillyoumakeit;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    public static final String TAG = MainActivity.class.getName();
     private GoogleMap mMap;
-    final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 123;
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+    private Marker currentMarker;
+    private final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 123;
+    private final long LOCATION_REFRESH_TIME = 1000;
+    private final float LOCATION_REFRESH_DISTANCE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestPermission();
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                updateMap(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+                Log.d(TAG, "Status Changed");
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                Log.d(TAG, "Location turned on");
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Log.d(TAG, "Location turned off");
+            }
+        };
+        
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    
+    private void statusCheck(){
+        if ( !mLocationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your Location seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog,  final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        requestPermission();
+        statusCheck();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location lastLocation = mLocationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            updateMap(lastLocation);
+        }
+
     }
 
-    private void requestPermission(){
+    private void updateMap(Location location) {
+        if (location == null) return;
+        if (currentMarker != null){
+            currentMarker.remove();
+        }
+        Log.d(TAG, location.toString() + "updated");
+        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14.0f));
+        MarkerOptions currentMarkerOptions = new MarkerOptions().position(currentLatLng).title("Current Location");
+        currentMarker = mMap.addMarker(currentMarkerOptions);
+    }
+
+    private void requestPermission() {
+        Log.v(TAG, "Requesting Permission");
         if (ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -62,6 +128,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             }
         } else {
             // DO Something
+            mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                    LOCATION_REFRESH_DISTANCE, mLocationListener);
+
         }
     }
 
@@ -73,13 +143,19 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+                    Log.v(TAG, "Permission Granted");
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-                    //Do Something
+                    if ((ContextCompat.checkSelfPermission(this,
+                            android.Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED)){
+                        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME,
+                                LOCATION_REFRESH_DISTANCE, mLocationListener);
+                    }
 
                 } else {
-
+                    Log.v(TAG, "Permission Denied");
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
                 }
